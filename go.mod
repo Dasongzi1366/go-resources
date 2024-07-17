014604218af684c29c72b34111c6d72b46b69d4a
Dasongzi1366 <13662618@qq.com>
2024-07-18 05:41:33 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Dasongzi1366/go-resources
+
+go 1.21.5
---
+++ resources.go
@@ -0,0 +1,33 @@
+package resources
+
+import _ "embed"
+
+//go:embed libc++_shared.so
+var Libcpp_shared []byte
+
+//go:embed libopencv_java4.so
+var Libopencv_java4 []byte
+
+//go:embed libpaddle.so
+var Libpaddle []byte
+
+//go:embed libpaddle_native.so
+var Libpaddle_native []byte
+
+//go:embed libyolov5.so
+var Libyolov5 []byte
+
+//go:embed libyolov5_native.so
+var Libyolov5_native []byte
+
+//go:embed ch_ppocr_mobile_v2.0_cls_opt.nb
+var Ch_ppocr_mobile_cls_opt []byte
+
+//go:embed ch_ppocr_mobile_v2.0_det_opt.nb
+var Ch_ppocr_mobile_det_opt []byte
+
+//go:embed ch_ppocr_mobile_v2.0_rec_opt.nb
+var Ch_ppocr_mobile_rec_opt []byte
+
+//go:embed ch_ppocr_keys.txt
+var Ch_ppocr_keys []byte
