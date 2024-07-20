705abd1a62803c0753d130e02171bd8ac9ff6721
Dasongzi1366 <13662618@qq.com>
2024-07-21 01:26:51 +0800

    Update resources

--- resources.go
+++ resources.go
@@ -19,15 +19,3 @@ var Libyolov5 []byte
 
 //go:embed libyolov5_native.so
 var Libyolov5_native []byte
-
-//go:embed ch_ppocr_mobile_v2.0_cls_opt.nb
-var Ch_ppocr_mobile_cls_opt []byte
-
-//go:embed ch_ppocr_mobile_v2.0_det_opt.nb
-var Ch_ppocr_mobile_det_opt []byte
-
-//go:embed ch_ppocr_mobile_v2.0_rec_opt.nb
-var Ch_ppocr_mobile_rec_opt []byte
-
-//go:embed ch_ppocr_keys.txt
-var Ch_ppocr_keys []byte
